8148aa9c644af4586d3331faeb783c9e79cd1c0d
Oddy Virgantara Putra <oddy@unida.gontor.ac.id>
2019-02-24 12:50:18 +0700

    init project

---
+++ Course 1/Contoh/Belajar/src/belajar/Belajar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author oddy
+ */
+public class Belajar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        JOptionPane.showMessageDialog(null, "Hello World");
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Materi Algoritmat dan Pemrograman
